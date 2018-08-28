fe89bec219367454f4802629fc6db71c29006b38
Sean Parsons <seanparsons@Seans-MacBook-Pro-3.local>
2018-08-27 20:10:14 -0400

    added delay in API calls to google

--- d2_automation.py
+++ d2_automation.py
@@ -1,5 +1,6 @@
 from googleplaces import GooglePlaces, types, lang
 import pandas as pd
+import time
 API_KEY = 'AIzaSyDMyNyQlAw_XMeUf9NTNQ7xE09Bo1k-t3U'
 
 google_places = GooglePlaces(API_KEY)
@@ -15,15 +16,24 @@ query_result = google_places.nearby_search(
 				location=location1, keyword=keyword1, 
 				radius=radius1)
 
+next_page = query_result.next_page_token
+
+print('Time to give Google\'s API a break.')
+
+time.sleep(2)
+
 if query_result.has_next_page_token:
-    print(query_result.next_page_token)
     query_result_second_page = google_places.nearby_search(location=location1, keyword=keyword1, 
-                                    radius=radius1, pagetoken=(query_result.next_page_token))
+                                    radius=radius1, pagetoken=query_result.next_page_token)
     responses = [query_result, query_result_second_page]
     
-    #if query_result_second_page.has_next_page_token:
-        #query_result_third_page = google_places.nearby_search(pagetoken=query_result.next_page_token)
-        #responses = [query_result, query_result_second_page, query_result_third_page]
+    print('Time for another break....')
+    time.sleep(2)
+
+    if query_result_second_page.has_next_page_token:
+        query_result_third_page = google_places.nearby_search(location=location1, keyword=keyword1, 
+                                    radius=radius1, pagetoken=query_result_second_page.next_page_token)
+        responses = [query_result, query_result_second_page, query_result_third_page]
 else:
     responses = [query_result]
 
@@ -46,11 +56,7 @@ for response in responses:
         #print(place.name)
         #print(place.geo_location)
         #print(place.place_id)
-
 print(data.head())
+print(data.shape)
 
-data.to_csv('d2_output.csv', encoding='utf-8')
-
-        
-
-
+data.to_csv('d2_' + keyword1 + '_in_' + location1 + '.csv', encoding='utf-8')
